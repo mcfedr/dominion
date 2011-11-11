1c8193417191ce67e17509ed2f3c2a3b98934db4
Fred Cox <mcfedr@gmail.com>
2011-11-11 03:54:53 +0200

    run in batches

--- client/test.js
+++ client/test.js
@@ -35,87 +35,93 @@ playersInGames = {
 
 var gamesToRun = 1000;
 
-var starts = gamesToRun;
-var finishes = gamesToRun;
+var run = function() {
 
-var start = function() {
-	starts--;
-	//console.log('games left to start: ' + starts);
-	//choose number of players
-	var players = 2 + Math.floor(Math.random() * 3);
-	playersInGames[players]++;
+	var starts = 0;
+	var finishes = 0;
+
+	var start = function() {
+		starts++;
+		//console.log('games left to start: ' + starts);
+		//choose number of players
+		var players = 2 + Math.floor(Math.random() * 3);
+		playersInGames[players]++;
 	
-	//choose players
-	var gameplayers = [];
-	var chosen = 0, i, h, num = 0;
-	do {
-		chosen++;
-		i = Math.floor(Math.random() * handlers.length);
-		h = handlers[i];
-		played[h.aiName]++;
-		gameplayers.push(h);
-	}
-	while(chosen < players);
-	
-	//checks when all the players have finished
-	var ended = gameplayers.length;
-	var checkEnded = function() {
-		if(--ended == 0) {
-			console.log('games left to finish ' + finishes);
-			if(--finishes == 0) {
-				finish();
-			}
+		//choose players
+		var gameplayers = [];
+		var chosen = 0, i, h, num = 0;
+		do {
+			chosen++;
+			i = Math.floor(Math.random() * handlers.length);
+			h = handlers[i];
+			played[h.aiName]++;
+			gameplayers.push(h);
 		}
-	};
+		while(chosen < players);
 	
-	//checks when all the players are ready to start
-	var readys = gameplayers.length;
-	var checkReady = function() {
-		if(--readys == 0) {
-			if(players < 4) {
-				ais[0].client.start();
+		//checks when all the players have finished
+		var ended = gameplayers.length;
+		var checkEnded = function() {
+			if(--ended == 0) {
+				//console.log('games left to finish ' + finishes);
+				if(++finishes == gamesToRun) {
+					finish();
+				}
 			}
-			if(starts > 0) {
-				start();
-			}
-		}
-	};
+		};
 	
-	var ais = [];
-	gameplayers.each(function(h) {
-		var ai = new h(host);
-		var ts = 0;
-		ai.on('welcome', checkReady);
-		ai.on('won', function(won) {
-			if(won) {
-				wins[h.aiName]++;
-				turns[h.aiName] += ts;
+		//checks when all the players are ready to start
+		var readys = gameplayers.length;
+		var checkReady = function() {
+			if(--readys == 0) {
+				if(players < 4) {
+					ais[0].client.start();
+				}
+				if(starts < gamesToRun) {
+					start();
+				}
 			}
-			checkEnded();
+		};
+	
+		var ais = [];
+		gameplayers.each(function(h) {
+			var ai = new h(host);
+			var ts = 0;
+			ai.on('welcome', checkReady);
+			ai.on('won', function(won) {
+				if(won) {
+					wins[h.aiName]++;
+					turns[h.aiName] += ts;
+				}
+				checkEnded();
+			});
+			ai.on('turn', function() {
+				ts++;
+			});
+			ais.push(ai);
 		});
-		ai.on('turn', function() {
-			ts++;
+	};
+
+	var finish = function() {
+		Object.each(wins, function(wins, name) {
+			console.log(name + ' won ' + ((wins / played[name]) * 100).round(2) + '%');
 		});
-		ais.push(ai);
-	});
+		Object.each(wins, function(wins, name) {
+			console.log(name + ' won with ' + (turns[name] / wins).round(2) + ' cards on average');
+		});
+		/*Object.each(wins, function(wins, name) {
+			console.log(name + ' won ' + wins);
+		});
+		Object.each(wins, function(wins, name) {
+			console.log(name + ' played ' + played[name]);
+		});
+		Object.each(playersInGames, function(val, key) {
+			console.log(val + ' ' + key + ' player games');
+		});*/
+		run();
+	};
+
+	start();
 };
 
-var finish = function() {
-	Object.each(wins, function(wins, name) {
-		console.log(name + ' won ' + ((wins / played[name]) * 100).round(2) + '%');
-	});
-	Object.each(wins, function(wins, name) {
-		console.log(name + ' won with ' + (turns[name] / wins).round(2) + ' cards on average');
-	});
-	/*Object.each(wins, function(wins, name) {
-		console.log(name + ' won ' + wins);
-	});
-	Object.each(wins, function(wins, name) {
-		console.log(name + ' played ' + played[name]);
-	});
-	Object.each(playersInGames, function(val, key) {
-		console.log(val + ' ' + key + ' player games');
-	});*/
-};
-
-start();
+run();
