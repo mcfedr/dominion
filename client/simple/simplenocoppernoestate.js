e7d377434e70e9f6f575e2d69f7532aae711dfd3
Fred Cox <fred@ekreative.com>
2011-11-18 11:36:38 +0000

    add header

--- client/simple/simplenocoppernoestate.js
+++ client/simple/simplenocoppernoestate.js
@@ -1,3 +1,21 @@
+/**
+*   This file is part of dominion-client.
+*   Copyright Fred Cox 2011
+*
+*   dominion-client is free software: you can redistribute it and/or modify
+*    it under the terms of the GNU General Public License as published by
+*    the Free Software Foundation, either version 3 of the License, or
+*    (at your option) any later version.
+*
+*    dominion-server is distributed in the hope that it will be useful,
+*    but WITHOUT ANY WARRANTY; without even the implied warranty of
+*    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*    GNU General Public License for more details.
+*
+*    You should have received a copy of the GNU General Public License
+*    along with dominion-client.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 var ai = require('./../basicai.js');
 var theCards = require('./../../server/cards.js');
 var rl = require('readline');
